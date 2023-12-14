de5bf1e7c7be00599fdcb2bc5069e0844b5d442b
unknown <smartkelvin66@gmail.com>
2023-12-14 15:37:17 +0100

    e-learning

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    outfit: require("./assets/fonts/Outfit-Black.ttf"),
+    "Outfit-Bold": require("./assets/fonts/Outfit-Bold.ttf"),
+    "Outfit-ExtraLight": require("./assets/fonts/Outfit-ExtraLight.ttf"),
+    "Outfit-Medium": require("./assets/fonts/Outfit-Medium.ttf"),
+  });
+
   return (
     <View style={styles.container}>
+      <Text style={{ fontFamily: "outfit" }}>
+        Open up App.js to start working on your app!
+      </Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
